6a7a0bc34801c1eded88fe5f81344e5356c2c2ff
Gretten <shhworkmail@yandex.ru>
2018-12-11 23:46:11 +0300

    next method added

--- mapmodule.js
+++ mapmodule.js
@@ -4,11 +4,36 @@ const module = (function() {
         const newScriptElement = document.createElement(options.el || 'script');
         const body = document.querySelector(options.container || 'body');
         newScriptElement.setAttribute(options.attr || 'src', options.link || 'https://api-maps.yandex.ru/1.1/index.xml');
-        return body.insertAdjacentElement(options.position || 'beforeend', newScriptElement);
+        body.insertAdjacentElement(options.position || 'beforeend', newScriptElement);
+        return newScriptElement;
+    };
+
+    function privateFiller(elements, text) {
+        const cityFiels = document.querySelectorAll(elements);
+            cityFiels.forEach(function(index) {
+            index.innerHtml = text;
+        })
     }
+
+    function privateFindAndOnload() {
+        const created = document.querySelectorAll('script');
+        created.forEach(function(index) {
+            if(index.src === 'https://api-maps.yandex.ru/1.1/index.xml') {
+                index.onload = function() {
+                    const cityFiels = document.querySelectorAll('.user-city');
+                    const city = YMaps.location.city;
+                    console.log(cityFiels, city)
+                    for(key of cityFiels) {
+                        key.innerHTML = city;
+                    }
+                };
+            };
+        });
+    };
 
     function publicInit(options) {
         privateCreateElement(options);
+        privateFindAndOnload();
     }
 
     return {
