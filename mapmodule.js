1e9398a2f19ea2c4897747279ca2829c0067cc72
Gretten <shhworkmail@yandex.ru>
2018-12-12 12:22:08 +0300

    architecture rebuilded

--- mapmodule.js
+++ mapmodule.js
@@ -1,6 +1,6 @@
 const module = (function() {
-    
-    function privateCreateElement(options) {
+
+    const privateCreateElement = options => {
         const newScriptElement = document.createElement(options.el || 'script');
         const body = document.querySelector(options.container || 'body');
         newScriptElement.setAttribute(options.attr || 'src', options.link || 'https://api-maps.yandex.ru/1.1/index.xml');
@@ -8,32 +8,29 @@ const module = (function() {
         return newScriptElement;
     };
 
-    function privateFiller(elements, text) {
-        const cityFiels = document.querySelectorAll(elements);
-            cityFiels.forEach(function(index) {
-            index.innerHtml = text;
-        })
+    const privateFillCities = options => {
+        const cityFiels = document.querySelectorAll(options.fill);
+        const city = options.object || YMaps.location.city;
+        console.log(cityFiels, city)
+        for (key of cityFiels) {
+            key.innerHTML = city;
+        }
     }
 
-    function privateFindAndOnload() {
-        const created = document.querySelectorAll('script');
-        created.forEach(function(index) {
-            if(index.src === 'https://api-maps.yandex.ru/1.1/index.xml') {
-                index.onload = function() {
-                    const cityFiels = document.querySelectorAll('.user-city');
-                    const city = YMaps.location.city;
-                    console.log(cityFiels, city)
-                    for(key of cityFiels) {
-                        key.innerHTML = city;
-                    }
+    const privateFindAndOnload = options => {
+        const created = document.querySelectorAll(options.el || 'script');
+        created.forEach((index) => {
+            if (index.src === options.link || 'https://api-maps.yandex.ru/1.1/index.xml') {
+                index.onload = () => {
+                    privateFillCities(options)
                 };
             };
         });
     };
 
-    function publicInit(options) {
+    const publicInit = options => {
         privateCreateElement(options);
-        privateFindAndOnload();
+        privateFindAndOnload(options);
     }
 
     return {
@@ -41,50 +38,3 @@ const module = (function() {
     }
 
 })()
-
-// const yandexInc = (function() {
-//     const ymapsSrc = 'https://api-maps.yandex.ru/1.1/index.xml';
-//     const cityContainerClassName = 'user-city';
-
-//     function privateCreateElement() {
-//         const newScriptElement = document.createElement('script');
-//         const body = document.querySelector('body');
-//         newScriptElement.setAttribute('src', ymapsSrc);
-//         return body.insertAdjacentElement('beforeend', newScriptElement);
-//     }
-
-//     function privateFilling() {
-//         const cityDetected = YMaps.location.city;
-//         const cityContainers = document.querySelectorAll(cityContainerClassName);
-//         for (let i = 0; e < cityContainers.length; i++) {
-//             cityContainers[i].innerHTML = "г. " + cityDetected;
-//         }
-//     }
-
-//     function publicInit(handler) {
-//         if (handler) {
-//             privateCreateElement();
-//             privateFilling();
-//         }
-//     }
-
-//     function publicSetContainerClassName(el) {
-//         return cityContainerClassName = el;
-//     }
-
-//     fucntion
-
-//     return {
-//         el: publicSetContainerClassName,
-//         init: publicInit
-//     }
-// })()
-
-// (function() {
-//     YMaps.onLoad(function() {
-//         var e,
-//             n = YMaps.location,
-//             t = document.getElementsByClassName('user-city');
-//         for (e = 0; e < t.length; e++) t[e].innerHTML = "г. " + n.city
-//     })
-// }())
