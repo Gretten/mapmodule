f658b9d42c790a850927db079c8184b859f42634
Gretten <shhworkmail@yandex.ru>
2018-12-13 21:58:15 +0300

    onload method refactored

--- mapmodule.js
+++ mapmodule.js
@@ -3,40 +3,36 @@ const module = (function() {
     const privateCreateElement = options => {
         const newScriptElement = document.createElement(options.el || 'script');
         const body = document.querySelector(options.container || 'body');
-        newScriptElement.setAttribute(options.attr || 'src', options.link || 'https://api-maps.yandex.ru/1.1/index.xml');
+        newScriptElement.setAttribute('src', options.link || 'https://api-maps.yandex.ru/1.1/index.xml');
+        newScriptElement.setAttribute(options.attr || 'data-ymaps', 'true');
         body.insertAdjacentElement(options.position || 'beforeend', newScriptElement);
         return newScriptElement;
     };
 
     const privateFillCities = options => {
         const cityFiels = document.querySelectorAll(options.fill);
-        const alter = /^\S+\s/;
+        const alter = /^\S+/;
         let city;
 
         if(options.object || YMaps) {
             if(YMaps.location.city) {
                 city = YMaps.location.city;
             } else {
-                city = YMaps.location.region.match(alter)[0].trim();
+                city = YMaps.location.region.match(alter)[0];
             }
         } else {
             city = 'Москва'
         }
-
         for (key of cityFiels) {
             key.innerHTML = city;
         }
     }
 
     const privateFindAndOnload = options => {
-        const created = document.querySelectorAll(options.el || 'script');
-        created.forEach((index) => {
-            if (index.src === options.link || 'https://api-maps.yandex.ru/1.1/index.xml') {
-                index.onload = () => {
-                    privateFillCities(options)
-                };
-            };
-        });
+        const created = document.querySelector('[data-ymaps]');
+        created.onload = () => {
+            privateFillCities(options)
+        };
     };
 
     const publicInit = options => {
